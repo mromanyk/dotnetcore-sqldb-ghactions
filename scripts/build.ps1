936ab31dac78158e002348034cca8ee678dccd79
Mikhail Romanyk <mikhail_romanyk@epam.com>
2021-11-24 18:42:06 +0300

    Test1 - fix script ps

--- scripts/build.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-param(
-    [Parameter()]
-    [string]$configuration,
-    [string]$pathApp
-)
-$solution_file = Join-Path $pathApp "webapp/DotNetCoreSqlDb.csproj"
-dotnet build $solution_file -c $configuration
-if ($LASTEXITCODE) { throw "Dotnet failed" }
