b9562e677aa9f23731b39d1328b9feba4dbb440a
Mikhail Romanyk <mikhail_romanyk@epam.com>
2021-11-24 18:25:02 +0300

    Test1 - add script ps

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,9 +4,6 @@ param(
     [string]$configuration,
     [string]$pathApp
 )
-
-$solution_file = Join-Path $pathApp "App/DotNetCoreSqlDb.csproj"
-
+$solution_file = Join-Path $pathApp "webapp/DotNetCoreSqlDb.csproj"
 dotnet build $solution_file -c $configuration
-
 if ($LASTEXITCODE) { throw "Dotnet failed" }
